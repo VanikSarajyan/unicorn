3a14a7124c8a5a74598c668094d12e42cf9c3f06
VanikSarajyan <vaniksarajyan@gmail.com>
2023-09-23 20:35:51 +0400

    add request/response abstarction over wsgi

--- unicorn/main.py
+++ unicorn/main.py
@@ -1,12 +1,52 @@
 from urllib import parse
+from wsgiref.headers import Headers
+from http.client import responses
 
 
-def app(env, start_response):
-    status = "200 OK"
-    headers = [("Content-Type", "text/html; charset=utf8")]
-    start_response(status, headers)
+class Request:
+    def __init__(self, env) -> None:
+        self.env = env
 
-    GET = parse.parse_qs(env["QUERY_STRING"])
-    name = GET.get("name", ["PyCon"])[0]
+    @property
+    def args(self) -> dict:
+        return {k: v[0] for k, v in parse.parse_qs(self.env["QUERY_STRING"]).items()}
 
-    return [f"<h1>Hello {name}!</h1>".encode("utf-8")]
+
+class Response:
+    def __init__(
+        self, response=None, status=200, charset="utf-8", content_type="text/html"
+    ) -> None:
+        self.response = [] if response is None else response
+        self.charset = charset
+        self.headers = Headers()
+        self.content_type = f"{content_type}; charset={charset}"
+        self.headers.add_header("content_type", self.content_type)
+        self._status = status
+
+    @property
+    def status(self):
+        status_string = responses.get(self._status, "UNKNOWN")
+        return f"{self._status} {status_string}"
+
+    def __iter__(self):
+        for k in self.response:
+            if isinstance(k, bytes):
+                yield k
+            else:
+                yield k.encode(self.charset)
+
+
+def unicorn(function):
+    def application(env, start_response):
+        request = Request(env)
+        response = function(request)
+        start_response(response.status, response.headers.items())
+        return response
+
+    return application
+
+
+@unicorn
+def app(request: Request) -> Response:
+    name = request.args.get("name", "PyCon")
+    return Response([f"<h1>Hello, {name}</h1>"])
